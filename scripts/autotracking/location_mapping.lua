f112d3e9eae020d4072669f79c5755d327217683
Thomas Barlow <github@mysterymayhem.co.uk>
2026-01-21 18:16:41 +0000

    Add Star Island precise sea locations

--- scripts/autotracking/location_mapping.lua
+++ scripts/autotracking/location_mapping.lua
@@ -283,8 +283,8 @@ LOCATION_MAPPING = {
     [0x2380f7] = "@The Great Sea/Flight Control Platform/Bird-Man Contest - First Prize",
     [0x2380f8] = "@The Great Sea/Flight Control Platform/Submarine",
 
-    [0x2380fa] = "@The Great Sea/Star Island/Lookout Platform",
-    [0x2380f9] = "@The Great Sea/Star Island/Cave",
+    [0x2380fa] = "@The Great Sea (Precise)/Star Island/Star Island - Lookout Platform/",
+    [0x2380f9] = "@The Great Sea (Precise)/Star Island/Star Island - Cave/",
 
     [0x2380fb] = "@The Great Sea/Star Belt Archipelago/Lookout Platform",
 
